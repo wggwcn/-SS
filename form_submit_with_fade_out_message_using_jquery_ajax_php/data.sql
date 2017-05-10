46a8b60e2ff9e5c4d594701f0225d45aaa97fb0d
wggwcn <282872666@qq.com>
2017-05-10 23:14:05 +0800

    Create data.sql

---
+++ form_submit_with_fade_out_message_using_jquery_ajax_php/data.sql
@@ -0,0 +1,12 @@
+--  
+ -- Table structure for table `tbl_form`  
+ --  
+ CREATE TABLE IF NOT EXISTS `tbl_form` (  
+  `id` int(11) NOT NULL AUTO_INCREMENT,  
+  `name` text NOT NULL,  
+  `message` text NOT NULL,  
+  PRIMARY KEY (`id`)  
+ ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;  
+ --  
+ -- Dumping data for table `tbl_form`  
+ --  
